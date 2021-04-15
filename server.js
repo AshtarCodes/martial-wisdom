e79a33248aa03a007ab37bb782f656c969711485
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-04-14 23:52:07 -0400

    get and post working, put in progress

--- server.js
+++ server.js
@@ -1,62 +1,72 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
-const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
+const PORT = process.env.PORT || 4444;
+require('dotenv').config()
 
+let db,
+    DB_URI = process.env.DB_STRING
 
-const PORT = process.env.PORT || 3000
-
-// connect to mongoDB Atlas
-const connectionString = 'mongodb+srv://Ash:PXRoKUkjhSTHvaVhs@cluster0.ej8ku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-.then(client => {
-    console.log('Connected to Database');
-
-    //create a database
-    const db = client.db('wushu-wisdom')
-
-    //create a collection within the database
-    const quotesCollection = db.collection('quotes')
-
-    //set EJS templating
-    app.set('view engine', 'ejs')
-
-    // Extracts data and adds to body property of request object (req.body)
-    app.use(bodyParser.urlencoded({ 'extended': true}))
-    
-    // READ operation / GET request for root file
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname,'index.html'))
-        
-        // READ operation - retrieve quotes from DB  // find() retrieves, toArray() organizes 
-        db.collection('quotes').find().toArray()
-        .then(results => {
-            // res.render(view, locals) or file, data
-            res.render('index.ejs', { quotes: results })
-        })
-        .catch(err => console.log(err))
+MongoClient.connect(DB_URI, {useUnifiedTopology: true})
+    .then(client => {
+        console.log('Connected to database');
+        //create or specify a database
+        db = client.db('wushu-wisdom') 
     })
+    .catch (err => console.log(err))
+
+//set EJS templating
+app.set('view engine', 'ejs')
+// make static files in the public folder visible to express
+app.use(express.static('public'))
+// Extracts urlencoded data and adds to body property of request object (req.body)
+app.use(express.urlencoded({ 'extended': true}))
+// Parser for json objects, adds to request.body
+app.use(express.json())
+
+    // READ operation / GET request for root file - retrieve available quotes
+app.get('/', (req, res) => {
+    // READ operation - retrieve quotes from DB  // find() retrieves, toArray() organizes 
+    db.collection('quotes').find().toArray()
+    .then(results => {
+        // res.render(view, locals) or file, data
+        console.log('rendering quotes from "quotes" collection');
+        let randIndex = Math.floor(Math.random() * (results.length - 1))
+        res.render('index.ejs', { quotes: results, randomIndex: randIndex })
+    })
+    .catch(err => console.log(err))
+})
     
     //CREATE operation / POST request for quotes
     app.post('/quotes', (req, res) => {
         //insert quotes into database quote collection
-        quotesCollection.insertOne(req.body)
+        db.collection('quotes').insertOne({name: req.body.name, quote: req.body.quote, likes: 0})
         .then(result => {
             // browser expecting response. Redirect browser to root
             res.redirect('/')
         })
-        .catch (err => console.log(err))
+        .catch(err => console.log(err))
+    })
+
+    app.put('/addOneLike', (req, res) => {
+        db.collection('quotes').insertOne({name: req.body.name, quote: req.body.quote, likes: req.body.likes}, {
+            $set: {
+                likes: (Number(req.body.likes) || 0) + 1,
+                upsert: true
+            }
+        })
+        .then(result => {
+            console.log('Added one like')
+            res.json('Like added')
+        }) 
+        .catch( err => console.error(err))
     })
     
 
-    app.listen(PORT, () => {
-        console.log(`Server running on ${PORT}`)
-    })
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
 })
-.catch (err => console.log(err))
+
+
 
