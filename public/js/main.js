45a83741d40e5f037eab66f9c18944f9884ecf8f
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-04-16 00:34:34 -0400

    delete working. Put in progress

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,45 @@
 document.querySelector('#delete').addEventListener('click', deleteQuote)
+document.querySelector('.quote-likes').addEventListener('click', addLikes)
 
-function deleteQuote () {
-    return fetch('/deleteQuote', {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'},
-        status: 200
+const quoteAuthor = document.querySelector('.quote-name');
+const quoteText = document.querySelector('.quote-text');
+const quoteLikes = document.querySelector('.quote-likes');
 
-    })
+async function addLikes() {
+    try {
+        const response = await fetch('addOneLike', {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: quoteAuthor,
+                quote: quoteText,
+                likes: quoteLikes
+            })            
+        })
+        const data = await response.json()
+        console.log(data);
+        location.reload()
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function deleteQuote () {
+    try{
+        const response = await fetch('/deleteQuote', {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: quoteAuthor,
+                quote: quoteText,
+                likes: quoteLikes
+            })    
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    } catch (err){
+        console.error(err);
+    }
 }
